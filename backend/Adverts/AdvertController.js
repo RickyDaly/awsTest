4fdd94be05c4907b6c87ee5b6e8a1b9569bcdb8d
ricky <ricky92d@gmail.com>
2024-01-14 18:32:25 +0000

    removed modules

--- backend/Adverts/AdvertController.js
+++ backend/Adverts/AdvertController.js
@@ -3,10 +3,10 @@ const routes = express.Router();
 const Advert = require('./Advert');
 // const multer = require('multer');
 // const jwt_decode = require('jwt-decode');
-const mongoose = require('mongoose');
-const { Router } = require('express');
-const { db, count } = require('./Advert');
-const e = require('express');
+// const mongoose = require('mongoose');
+// const { Router } = require('express');
+// const { db, count } = require('./Advert');
+// const e = require('express');
 
 routes.get('/', function(req, res){
     Advert.find()
